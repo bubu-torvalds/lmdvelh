304b9ab21d21947005c2eaa8c43aa6af367e86c3
Maxime Buttard <maxime.buttard@gmail.com>
2017-06-06 15:44:45 +0200

    dancefloor code ok

--- Assets/Scripts/Controllers/DancefloorControllers/DancefloorController2.cs
+++ Assets/Scripts/Controllers/DancefloorControllers/DancefloorController2.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DancefloorController2 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Wedding wedding;
+    public Text desc;
+    private Scene currentScene;
+
+    // Use this for initialization
+    void Start() {
+
+        wedding = LoadXml.LoadXmlFile();
+
+        currentScene = wedding.getScenes().Find(x => x.getCode() == "D2");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (currentScene.getCode() == "D2") {
+            d2();
+        } else if (currentScene.getCode() == "D2") {
+            SceneManager.LoadScene("Dancefloor2");
+        } else if (currentScene.getCode() == "D") {
+            SceneManager.LoadScene("Dancefloor");
+        } 
+    }
+
+    void d2() {
+
+        desc.text = PrintText.printText(currentScene);
+
+        if (Input.GetKeyDown(KeyCode.Keypad1)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "D2");
+
+        } else if (Input.GetKeyDown(KeyCode.Keypad2)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "D");
+
+        } 
+    }
 }
--- Assets/Scripts/Controllers/DancefloorControllers/DancefloorController3.cs
+++ Assets/Scripts/Controllers/DancefloorControllers/DancefloorController3.cs
@@ -1,16 +1,102 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DancefloorController3 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Wedding wedding;
+    public Text desc;
+    private Scene currentScene;
+
+    // Use this for initialization
+    void Start() {
+
+        wedding = LoadXml.LoadXmlFile();
+
+        currentScene = wedding.getScenes().Find(x => x.getCode() == "D3");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (currentScene.getCode() == "D3") {
+            d3();
+        } else if (currentScene.getCode() == "D1") {
+            SceneManager.LoadScene("Dancefloor1");
+        } else if (currentScene.getCode() == "D2") {
+            SceneManager.LoadScene("Dancefloor2");
+        } else if (currentScene.getCode() == "D4") {
+            SceneManager.LoadScene("Dancefloor4");
+        } else if (currentScene.getCode() == "C") {
+            SceneManager.LoadScene("Chalet");
+        } else if (currentScene.getCode() == "T") {
+            SceneManager.LoadScene("Table");
+        } else if (currentScene.getCode() == "J") {
+            SceneManager.LoadScene("Jeux");
+        } else if (currentScene.getCode() == "F") {
+            SceneManager.LoadScene("Foret");
+        } else if (currentScene.getCode() == "B") {
+            SceneManager.LoadScene("Bar");
+        } else if (currentScene.getCode() == "M") {
+            SceneManager.LoadScene("Molkky");
+        } else if (currentScene.getCode() == "S") {
+            SceneManager.LoadScene("BBQ");
+        } else if (currentScene.getCode() == "DJ") {
+            SceneManager.LoadScene("DJ");
+        }
+    }
+
+    void d3() {
+
+        desc.text = PrintText.printText(currentScene);
+
+        if (Input.GetKeyDown(KeyCode.Keypad1)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "D1");
+
+        } else if (Input.GetKeyDown(KeyCode.Keypad2)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "D2");
+
+        } else if (Input.GetKeyDown(KeyCode.Keypad3)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "D4");
+
+        } else if (Input.GetKeyDown(KeyCode.Keypad4)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "C");
+
+        } else if (Input.GetKeyDown(KeyCode.C)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "T");
+
+        } else if (Input.GetKeyDown(KeyCode.T)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "J");
+
+        } else if (Input.GetKeyDown(KeyCode.J)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "F");
+
+        } else if (Input.GetKeyDown(KeyCode.F)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "B");
+
+        } else if (Input.GetKeyDown(KeyCode.B)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "M");
+
+        } else if (Input.GetKeyDown(KeyCode.M)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "S");
+
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "DJ");
+
+        } 
+    }
 }
