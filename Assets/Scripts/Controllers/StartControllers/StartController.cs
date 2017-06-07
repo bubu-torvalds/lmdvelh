5d0ff7bc0125fdcf3907218dc56c1b6ec72a2b76
Maxime Buttard <maxime.buttard@gmail.com>
2017-06-07 15:59:56 +0200

    On touche au but

---
+++ Assets/Scripts/Controllers/StartControllers/StartController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartController : MonoBehaviour {
+
+    public Wedding wedding;
+    public Text desc;
+    private Scene currentScene;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    void Start() {
+
+        wedding = LoadXml.LoadXmlFile();
+
+        audioSource.Play();
+
+        currentScene = wedding.getScenes().Find(x => x.getCode() == "ST");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (currentScene.getCode() == "ST") {
+            st();
+        } else if (currentScene.getCode() == "C") {
+            SceneManager.LoadScene("Chalet");
+        } 
+    }
+
+    void st() {
+
+        desc.text = PrintText.printText(currentScene);
+
+        if (Input.GetKeyDown(KeyCode.Keypad1)) {
+
+            currentScene = wedding.getScenes().Find(x => x.getCode() == "C");
+
+        } 
+    }
+}
