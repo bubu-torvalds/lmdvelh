97574a2d89124fba1a78692e6f1b18bec2e34e9f
Maxime Buttard <maxime.buttard@gmail.com>
2017-12-01 14:41:36 -0500

    possibilité de rejouer

--- Assets/Scripts/Controllers/EndGameControllers/EndGameController.cs
+++ Assets/Scripts/Controllers/EndGameControllers/EndGameController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class EndGameController : MonoBehaviour {
-    
+
+    public Wedding wedding;
     public Image image;
+
+    private Scene currentScene;
 
     // Use this for initialization
     void Start () {
-        
+
         image.canvasRenderer.SetAlpha(0.0f);
 
     }
@@ -19,6 +23,13 @@ public class EndGameController : MonoBehaviour {
 
         // La ligne suivante est SALE mais CrossFadeAlpha ne fonctionne pas pour d'obscure raison dans ce sens donc dirty code is dirty.
         image.canvasRenderer.SetAlpha(image.canvasRenderer.GetAlpha() + 0.0003f);
+
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            
+            SceneManager.LoadScene("Start");
+
+        }
 
     }
 }
